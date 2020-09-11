3b26d288879ad6babed1dcc47d37d610e471d500
martinechtner <martin.echtner@gmail.com>
2020-09-11 12:15:28 -0600

    Added increment method.

--- app/models/user_increment.rb
+++ app/models/user_increment.rb
@@ -3,4 +3,8 @@ class UserIncrement < ApplicationRecord
 
   validates_presence_of :user
   validates_numericality_of :current, greater_than: 0, only_integer: true
+
+  def increment
+    self.update(current: current + 1)
+  end
 end
